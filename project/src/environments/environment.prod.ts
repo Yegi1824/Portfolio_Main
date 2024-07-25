a062187f6e103bcb69ae893b982b75ba42f78faa
Yehor <egorkaishchenko@gmail.com>
2024-07-25 10:24:56 +0300

    deploy on heroku

--- project/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  sGoogleApiKey: 'AIzaSyBCo-dfE8Yh92tJ5MoAMwBcj9--aqYaLqk',
-  sTelegramBotToken: '7032326878:AAFRo2h6bdSvRgebLrJAkb5fxFOgbAGvW1w'
-};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  sGoogleApiKey: 'AIzaSyBCo-dfE8Yh92tJ5MoAMwBcj9--aqYaLqk',
+  sTelegramBotToken: '7032326878:AAFRo2h6bdSvRgebLrJAkb5fxFOgbAGvW1w'
+};
